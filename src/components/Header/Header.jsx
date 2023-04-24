802a75c1ef5091bfe5742323b760c9e069a9d493
Abid Hasan <108365863+AbidReal@users.noreply.github.com>
2023-04-24 18:20:18 +0600

    61-5 done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import './Header.css';
-import logo from '../../images/Logo.svg';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div>
-                <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/login">Login</Link>
-            </div>
-        </nav>
-    );
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogout = () => {
+    logOut()
+      .then((result) => {})
+      .catch((error) => console.error(error));
+  };
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <Link to="/">Shop</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/login">LogIn</Link>
+        <Link to="/signup">SignUp</Link>
+        {user && (
+          <span className="text-white">
+            Welcome {user.email} <button onClick={handleLogout}>Log out</button>
+          </span>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOJ4WQV0f3Et84QKkqOtVgSclRzKWEJiQ",
+  authDomain: "ema-john-with-firebase-a-de927.firebaseapp.com",
+  projectId: "ema-john-with-firebase-a-de927",
+  storageBucket: "ema-john-with-firebase-a-de927.appspot.com",
+  messagingSenderId: "178807793597",
+  appId: "1:178807793597:web:b9a362b794cd2e9ba894d4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
